52e4df88d5908f81416dc780a54d7d89fcc4e280
ritik dutta <ritikduttagd@gmail.com>
2025-11-29 19:40:48 +0530

    creating chat flow

---
+++ app/chat_flow.py
@@ -0,0 +1,61 @@
+import os
+from typing import Dict
+from dotenv import load_dotenv
+
+from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+from .config import GENAI_MODEL, GOOGLE_API_KEY
+
+# set API key for Google GenAI client (auto set by env var, but just to be sure [not to be dont on production])
+if GOOGLE_API_KEY:
+    os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+
+llm = init_chat_model(
+    f"google_genai:{GENAI_MODEL}",
+    temperature=0.3,
+)
+
+# prompt
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are BOT-GPT, a helpful assistant. be consise and friendly"
+        ),
+        MessagesPlaceholder("history"),
+        ("human", "{input}"),
+    ]
+)
+
+# LCEL chain (new way to bind in langchain)
+base_chain = prompt | llm
+
+# in-memory session store
+_session_store: Dict[str, InMemoryChatMessageHistory] = {}
+
+def get_session_history(session_id: str) -> InMemoryChatMessageHistory:
+    if session_id not in _session_store:
+        _session_store[session_id] = InMemoryChatMessageHistory()
+    return _session_store[session_id]
+
+chain_with_history = RunnableWithMessageHistory(
+    base_chain, 
+    get_session_history=get_session_history,
+    input_message_key="input",
+    history_message_key="history",
+)
+
+def ask_bot(session_id: str, user_input: str):
+    """ handles the chat endpoint """
+    config = {"configurable": {"session_id": session_id}}
+
+    response = chain_with_history.invoke(
+        {"input": user_input},
+        config=config,
+    )
+
+    # response is an AIMessage
+    return response.content
