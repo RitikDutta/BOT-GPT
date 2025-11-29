62c31f29dbe179313c4ade00a06a715a03e36001
ritik dutta <ritikduttagd@gmail.com>
2025-11-30 04:57:43 +0530

    added config to database

--- app/config.py
+++ app/config.py
@@ -3,6 +3,13 @@ import os
 GENAI_MODEL = os.getenv("GENAI_MODEL", "gemini-2.5-flash-lite")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = int(os.getenv("DB_PORT", "3306"))
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_NAME = os.getenv("DB_NAME", "bot_gpt_test")
+
 # later we can add more models , user preferences , etc
 # this is a small file as of now but later in the production application there will be many settings in this config in produciton applicaion
 # so it will be easy to manage.
